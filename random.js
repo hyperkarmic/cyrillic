4e4b2abbbe91974727da54ad1ec0cec2ed44b23a
hyperkarmic <randy_pan@yahoo.com>
2025-04-18 16:55:59 +0100

    logic using event  handlers

--- random.js
+++ random.js
@@ -1,18 +1,47 @@
 
 
 const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+let item = alphabet[Math.floor(Math.random()*alphabet.length)]
 
-const item = alphabet[Math.floor(Math.random()*alphabet.length)]
+ const refreshItem = () =>{
+ item = alphabet[Math.floor(Math.random()*alphabet.length)]
+   text.style.color = "black"
 
-  console.log(alphabet)
-let text = document.getElementById("test")
+text.innerHTML = item
+
+
+ }
+
+
+  console.log("bastards!!!!")
+const fu = () =>{
+  refreshItem()
+
+}
+
+
+
+const itemmaker = () =>{}
+let text = document.getElementById("character")
 text.innerHTML = item
 document.addEventListener("keydown", (event) => {
-  console.log("boo")
 
-// text.style.color = "red";
-text.style.color = "green";
-});
+  if(item != event.key){
+      text.style.color = "red"
 
+    setTimeout(fu, 5000);
+    }
+  if(item == event.key){
+
+
+ text.style.color = "green"
+
+    setTimeout(fu, 5000);
+    }
+})
+
+
+
+//text.style.color = "green";
 
 console.log(alphabet)
